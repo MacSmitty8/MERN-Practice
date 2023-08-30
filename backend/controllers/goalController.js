357d4999f102ae22e0d2975de93a5bf0d143d8e0
MacreenSmith <macreensmith@gmail.com>
2023-08-30 17:00:51 -0400

    Adjusted the routes to make sure they functioned properly.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,15 @@
 const asyncHandler = require('express-async-handler')
+
+const Goal = require('../models/goalModel')
 //@des  get goals
 //@routw   Get /api/goals
 
 //@access Private
 
 const getGoals = asyncHandler(async(req, res) =>{
-    res.status(200).json({message: "Get Goals"});
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 });
 
 //@des  get goals
@@ -14,10 +18,14 @@ const getGoals = asyncHandler(async(req, res) =>{
 //@access Private
 
 const setGoals = asyncHandler(async(req, res) =>{
-    if(req.body.text){
+    if(!req.body.text){
         res.status(400);
         throw new Error("Please add a text field");
     }
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 });
 
 //@des  get goals
@@ -30,11 +38,31 @@ const setGoals = asyncHandler(async(req, res) =>{
 //@access Private
 
 const updateGoals = asyncHandler(async(req, res) =>{
-    res.status(200).json({message: `Update goal ${req.params.id}`});
+    //find the id
+    const goal = await Goal.findById(req.params.id)
+
+    //check if goal exists 
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    //finding the goal and creating if it doesn't exist
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedGoal);
 });
 
 const deleteGoals = asyncHandler(async(req, res) =>{
-    res.status(200).json({message: `Delete goal ${req.params.id}`});
+    //find the id
+    const goal = await Goal.findByIdAndDelete(req.params.id)
+
+    //check if goal exists
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    // await goal.findByIdandRemove(req.params.id)
+    res.status(200).json({id: req.params.id});
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-console.log('Hello World')
 const express = require('express')
-const dotenv = require('dotenv').config
+const colors = require('colors') //not required
+const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddle')
 const port = process.env.PORT || 5000
-
+const connectDB = require('./config/db')
 const app = express()
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 //user resource
-
+connectDB()
 //....
-
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () =>{
     console.log(`server started on ${port}`);
