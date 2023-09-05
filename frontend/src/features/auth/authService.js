02cebd3c0827e74145075e53f72c05ae6768b2f5
MacreenSmith <macreensmith@gmail.com>
2023-09-05 15:29:20 -0400

    Added changes to authSlice, and register.jsx

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,24 +1,38 @@
 import axios from 'axios'
 
-//making a call to out api/backend
-
 const API_URL = '/api/users/'
 
-const register = async(userData) => {
-    const response = await axios.post(API_URL, userData)
+// Register user
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData)
 
-    if(response.data){
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-    return response.data
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
+}
+
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData)
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
 }
 
 // Logout user
 const logout = () => {
-    localStorage.removeItem('user')
-  }
-const authService ={
-    register,
+  localStorage.removeItem('user')
+}
+
+const authService = {
+  register,
+  logout,
+  login,
 }
 
 export default authService
